f16da5be054a63ac0f90dfde25a65e7a0a608759
lie544 <Minecrafter622@yahoo.com>
2016-09-29 19:27:06 -0400

    finsihed project

---
+++ Assets/p2controls.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class p2controls : MonoBehaviour {
+
+    public KeyCode moveUp = KeyCode.I;
+    public KeyCode moveDown = KeyCode.K;
+    public float speed = 10.0f;
+    private Rigidbody2D rb2d;
+
+    // Use this for initialization
+   
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(moveUp))
+        {
+            var vel = rb2d.velocity;
+            vel.y = speed;
+            rb2d.velocity = vel;
+        }
+        else if (Input.GetKey(moveDown))
+        {
+            var vel = rb2d.velocity;
+            vel.y = -1 * speed;
+            rb2d.velocity = vel;
+        }
+        else if (!Input.anyKey)
+        {
+            var vel = rb2d.velocity;
+            vel.y = 0.0f;
+            rb2d.velocity = vel;
+        }
+    
+    var reset = rb2d.velocity;
+    reset.x = 0;
+    rb2d.velocity = reset;
+
+    }
+}
+
+
+
+
+
+
